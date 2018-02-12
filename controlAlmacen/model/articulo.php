2c8b0daa14ea22cf883e7d0fd261b234a4f46ed0
jesusrv1103 <jesus21c.jrv@gmail.com>
2018-02-12 16:00:43 -0600

    necesario

--- controlAlmacen/model/articulo.php
+++ controlAlmacen/model/articulo.php
@@ -18,17 +18,50 @@ class Articulo
 
     public function Listar()
     {
-      
+      try
+        {
+            $result = array();
+
+            $stm = $this->pdo->prepare("SELECT * FROM articulos");
+            $stm->execute();
+
+            return $stm->fetchAll(PDO::FETCH_OBJ);
+        }
+        catch(Exception $e)
+        {
+            die($e->getMessage());
+        }
     }
 
     public function Obtener($id)
     {
+        try 
+        {
+            $stm = $this->pdo
+                      ->prepare("SELECT * FROM  articulos WHERE idArticulo = ?");
+                      
+
+            $stm->execute(array($id));
+            return $stm->fetch(PDO::FETCH_OBJ);
+        } catch (Exception $e) 
+        {
+            die($e->getMessage());
+        }
        
     }
 
     public function Eliminar($id)
     {
-        
+        try 
+        {
+            $stm = $this->pdo
+                        ->prepare("DELETE FROM articulos WHERE idArticulo = ?");                    
+
+            $stm->execute(array($id));
+        } catch (Exception $e) 
+        {
+            die($e->getMessage());
+        }   
     }
 
     public function Actualizar($data)
