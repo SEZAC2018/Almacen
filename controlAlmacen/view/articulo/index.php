1c8884c86cffd2779afc3364cfdc12d76b4405b5
jesusrv1103 <jesus21c.jrv@gmail.com>
2018-03-01 15:44:45 -0600

    necesario

--- controlAlmacen/view/articulo/index.php
+++ controlAlmacen/view/articulo/index.php
@@ -1,73 +1,136 @@
-      <!--\\\\\\\ contentpanel start\\\\\\-->
-      <div class="pull-left breadcrumb_admin clear_both">
-        <div class="pull-left page_title theme_color">
-          <h1>Catálogos</h1>
-          <h2 class="">Articulo</h2>
-        </div>
-        <div class="pull-right">
-          <ol class="breadcrumb">
-            <li><a href="?c=Inicio">Inicio</a></li>
-            <li class="active">Articulo</a></li>
-          </ol>
-        </div>
-      </div>
-      <div class="container clear_both padding_fix">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="block-web">
-              <div class="header">
-                <div class="row" style="margin-top: 15px; margin-bottom: 12px;">
-                  <div class="col-sm-7">
-                    <div class="actions"> </div>
-                    <h2 class="content-header theme_color" style="margin-top: -5px;">&nbsp;&nbsp;Articulos</h2>
-                  </div>
-                  <div class="col-md-5">
-                    <div class="btn-group pull-right">
-                      <b>
-
-                        <div class="btn-group" style="margin-right: 10px;">
-                          <a class="btn btn-sm btn-success tooltips" href="?c=Articulo&a=Crud" style="margin-right: 10px;" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Registrar nueva Direccion"> <i class="fa fa-plus"></i> Registrar </a>
-
-                        </div>
-
-                      </b>
-                    </div>
-                  </div>
+<div class="pull-left breadcrumb_admin clear_both">
+  <div class="pull-left page_title theme_color">
+    <h1>Inicio</h1>
+    <h2 class="">Articulo</h2>
+  </div>
+  <div class="pull-right">
+    <ol class="breadcrumb">
+      <li><a href="?c=Inicio">Inicio</a></li>
+      <li><a href="?c=localidad">Articulo</a></li>
+      <li class="active">Alta Articulo</li>
+    </ol>
+  </div>
+</div>
+<div class="container clear_both padding_fix">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="block-web">
+        <div class="header">
+          <div class="row" style="margin-top: 15px; margin-bottom: 12px;">
+            <div class="col-sm-8">
+              <div class="actions"> </div>
+              <h2 class="content-header theme_color" style="margin-top: -5px;">Registrar Articulo</h2>
+            </div>
+            <div class="col-md-4">
+              <div class="btn-group pull-right">
+                <div class="actions"> 
                 </div>
               </div>
+            </div>    
+          </div>
+        </div>
+        <div class="porlets-content">
+          <form action="" method="POST" class="form-horizontal row-border"  parsley-validate novalidate>
 
 
-              <div class="porlets-content">
-                <div class="row">
-                  
-                  <div class="col-md-2">
-                    <div class="panel">
-                      
-                      <div id='external-events'>
-                        <div class="fc-heading">
-                          <h5>Draggable Events</h5>
-                        </div>
-                        <div class="fc-body">
-                          <div class='external-event'>My Event 1</div>
-                          <div class='external-event'>My Event 2</div>
-                          <div class='external-event'>My Event 3</div>
-                          <div class='external-event'>My Event 4</div>
-                          <div class='external-event'>My Event 5</div>
-                          <p>
-                            <input type='checkbox' id='drop-remove' /> <label for='drop-remove'>remove after drop</label>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-10">
-                    <div class="block-web">
-                      <div id='calendar'></div>
-                    </div>
-                  </div>
-                  
-                </div>
-              </div><!--/porlets-content-->
-            </div><!--/block-web-->
-          </div><!--/col-md-12-->
-        </div><!--/row-->
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Nombre Articulo<strog class="theme_color">*</strog></label>
+              <div class="col-sm-6">
+
+               <input onchange="mayus(this);" type="text" class="form-control" maxlength="70" placeholder="Ingrese el Nombre del Articulo">
+             </div>
+           </div><!--/form-group-->
+           <div class="form-group">
+
+             <label class="col-sm-3 control-label">Cantidad articulo: <strog class="theme_color">*</strog></label>
+             <div class="col-sm-6">
+               <input onkeypress="return soloNumeros(event);" type="text" class="form-control" maxlength="12" placeholder="Ingrese la cantidad del Articulo">
+             </div>
+           </div><!--/form-group-->
+           <div class="form-group">
+            <label class="col-sm-3 control-label">Almacen:<strog class="theme_color">*</strog></label>
+            <div class="col-sm-6">
+              <select class="form-control" name="ambito" required id="ambito">
+                <option value="1"> 
+                 Almacen 1             
+               </option>
+               <option value="2"> 
+                 Almacen 2            
+               </option>
+             </select>
+           </div>
+         </div><!--/form-group-->
+
+         <div class="form-group">
+          <label class="col-sm-3 control-label">Unidad de Medida:<strog class="theme_color">*</strog></label>
+          <div class="col-sm-6">
+            <select class="form-control" name="ambito" required id="ambito">
+              <option value="1"> 
+               Caja             
+             </option>
+             <option value="2"> 
+               Paquete            
+             </option>
+
+             <option value="3"> 
+               Pieza           
+             </option>
+           </select>
+         </div>
+       </div><!--/form-group-->
+
+       <div class="form-group">
+        <label class="col-sm-3 control-label">Tipo Articulo:<strog class="theme_color">*</strog></label>
+        <div class="col-sm-6">
+          <select class="form-control" name="ambito" required id="ambito">
+            <option value="1"> 
+              Consumible          
+            </option>
+            <option value="2"> 
+             Papeleria            
+           </option>
+
+
+         </select>
+       </div>
+     </div><!--/form-group-->
+
+
+     <div class="form-group">
+
+      <label class="col-sm-3 control-label">Fecha Caducidad: <strog class="theme_color">*</strog></label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control mask" data-inputmask="'alias': 'date'">
+      </div>
+    </div><!--/form-group-->
+
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label">Partida:<strog class="theme_color">*</strog></label>
+      <div class="col-sm-6">
+        <select class="form-control" name="ambito" required id="ambito">
+          <option value="1"> 
+            2111          
+          </option>
+          <option value="2"> 
+           2121            
+         </option>
+
+
+       </select>
+     </div>
+   </div><!--/form-group-->
+
+
+   <div class="form-group">
+    <div class="col-sm-offset-7 col-sm-5">
+      <button type="submit" class="btn btn-primary">Guardar</button>
+      <a href="?c=Articulo" class="btn btn-default"> Cancelar</a>
+    </div>
+  </div><!--/form-group-->
+</form>
+</div><!--/porlets-content-->
+</div><!--/block-web-->
+</div><!--/col-md-12-->
+</div><!--/row-->
+</div><!--/container clear_both padding_fix--> 
