96467136e11a41e9a5768e8651516047a03adb11
jesusrv1103 <jesus21c.jrv@gmail.com>
2018-03-02 15:14:01 -0600

    necesario

--- controlAlmacen/view/almacenes/index.php
+++ controlAlmacen/view/almacenes/index.php
@@ -51,90 +51,77 @@
                       </tr>
                     </thead>
                     <tbody>
-
+                     <?php foreach($this->model->Listar() as $r): ?>
                       <tr class="gradeA">
+                        <td> <?php echo $r->nombre; ?></td>
 
-                        <td>Almacen 1</td>
+                        <td class="center">
 
-                      </td>
+                          <a href="#" class="btn btn-primary btn-sm" role="button"><i class="fa fa-edit"></i></a>
 
-                      <td class="center">
-                        <a href="RegistrarAlmacen.php" class="btn btn-primary btn-sm" role="button"><i class="fa fa-edit"></i></a>
-                      </td>
-                      <td class="center">
-                        <a onclick="eliminarDireccion(<?php echo $r->idDireccion;?>);" class="btn btn-danger btn-sm" href="#modalEliminar"  data-toggle="modal" data-target="#modalEliminar" role="button"><i class="fa fa-eraser"></i></a>
-                      </td>
+                        </td>
+                        <td class="center">
+                          <a onclick="eliminarMunicipio(<?php echo $r->idMunicipio;?>);" class="btn btn-danger btn-sm" href="#modalEliminar" style="margin-right: 10px;"  data-toggle="modal" data-target="#modalEliminar" role="button"><i class="fa fa-eraser"></i></a>
+                        </td>
+                
+                      </tr>
+                    <?php endforeach; ?>
 
-                    </tr>
 
-                    <tr class="gradeA">
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th>Nombre</th>
 
-                      <td>Almacen 2</td>
-
-                      <td class="center">
-                        <a href="index.php?c=Direccion&a=Crud&idDireccion=<?php echo $r->idDireccion ?>" class="btn btn-primary btn-sm" role="button"><i class="fa fa-edit"></i></a>
-                      </td>
-                      <td class="center">
-                        <a onclick="eliminarDireccion(<?php echo $r->idDireccion;?>);" class="btn btn-danger btn-sm" href="#modalEliminar"  data-toggle="modal" data-target="#modalEliminar" role="button"><i class="fa fa-eraser"></i></a>
-                      </td>
+                      <td><center><b>Editar</b></center></td>
+                      <td><center><b>Borrar</b></center></td>
 
                     </tr>
-                  </tr>
+                  </tfoot>
+                </table>
+              </div><!--/table-responsive-->
+            </div><!--/porlets-content-->
+          </div><!--/block-web-->
+        </div><!--/col-md-12-->
+      </div><!--/row-->
 
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <th>Nombre</th>
-
-                    <td><center><b>Editar</b></center></td>
-                    <td><center><b>Borrar</b></center></td>
-
-                  </tr>
-                </tfoot>
-              </table>
-            </div><!--/table-responsive-->
-          </div><!--/porlets-content-->
-        </div><!--/block-web-->
-      </div><!--/col-md-12-->
-    </div><!--/row-->
-
-    <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content  panel default red_border horizontal_border_1">
-          <div class="modal-body">
-            <div class="row">
-              <div class="block-web">
-                <div class="header">
-                  <h3 class="content-header theme_color">&nbsp;Eliminar Municipio</h3>
-                </div>
-                <div class="porlets-content" style="margin-bottom: -50px;">
-                  <h4>¿Esta segúro que desea eliminar el Municipio</h4>
-                </div><!--/porlets-content-->
-              </div><!--/block-web-->
+      <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content  panel default red_border horizontal_border_1">
+            <div class="modal-body">
+              <div class="row">
+                <div class="block-web">
+                  <div class="header">
+                    <h3 class="content-header theme_color">&nbsp;Eliminar Municipio</h3>
+                  </div>
+                  <div class="porlets-content" style="margin-bottom: -50px;">
+                    <h4>¿Esta segúro que desea eliminar el Municipio</h4>
+                  </div><!--/porlets-content-->
+                </div><!--/block-web-->
+              </div>
             </div>
-          </div>
-          <div class="modal-footer" style="margin-top: -10px;">
-            <div class="row col-md-5 col-md-offset-7" style="margin-top: -5px;">
-              <form action="?c=Municipio&a=Eliminar" enctype="multipart/form-data" method="post">
-                <input type="hidden" name="idMunicipio" id="txtIdMunicipio">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                <button type="submit" class="btn btn-danger">Eliminar</button>
-              </form>
+            <div class="modal-footer" style="margin-top: -10px;">
+              <div class="row col-md-5 col-md-offset-7" style="margin-top: -5px;">
+                <form action="?c=Municipio&a=Eliminar" enctype="multipart/form-data" method="post">
+                  <input type="hidden" name="idMunicipio" id="txtIdMunicipio">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                  <button type="submit" class="btn btn-danger">Eliminar</button>
+                </form>
+              </div>
             </div>
-          </div>
-        </div><!--/modal-content-->
-      </div><!--/modal-dialog-->
-</div><!--/modal-fade-->
+          </div><!--/modal-content-->
+        </div><!--/modal-dialog-->
+      </div><!--/modal-fade-->
 
-<script>
-
+      <script>
 
 
-  eliminarDireccion = function(idMunicipio){
-   
-    $('#txtIdMunicipio').val(idMunicipio);
-  };
-   deshabilitar = function (){
-  $('#btnImportar').attr("disabled", true);
-}
-</script>
+
+        eliminarDireccion = function(idMunicipio){
+
+          $('#txtIdMunicipio').val(idMunicipio);
+        };
+        deshabilitar = function (){
+          $('#btnImportar').attr("disabled", true);
+        }
+      </script>
